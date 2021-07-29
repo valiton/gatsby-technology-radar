3c167b1f975f06578a82b54f60b8d491e5e03a45
Uwe Jäger <uwej711@googlemail.com>
2021-07-29 07:54:20 +0200

    try to fix cache issues

--- packages/gatsby-transformer-technology-radar/src/gatsby-node.js
+++ packages/gatsby-transformer-technology-radar/src/gatsby-node.js
@@ -1,41 +1,42 @@
-const {createRadar} = require('@valiton/technology-radar');
+const {createRadar} = require(`@valiton/technology-radar`);
+
+const unstable_shouldOnCreateNode = ({node}, pluginOptions) => (node.internal.type === pluginOptions.nodeType);
 
 const onCreateNode = (
   {node, actions, createNodeId, createContentDigest},
   pluginOptions
 ) => {
-  if (node.internal.type === pluginOptions.nodeType) {
-    let radar;
+  let radar;
 
-    if (pluginOptions.layout) {
-      radar = createRadar(
-        pluginOptions.radarName,
-        node.items,
-        pluginOptions.layout
-      );
-    } else {
-      radar = createRadar(
-        pluginOptions.radarName,
-        node.items,
-        pluginOptions.layout
-      );
-    }
-
-    const {createNode, createParentChildLink} = actions;
-
-    const radarNode = {
-      ...radar,
-      id: createNodeId(`${node.id} Radar`),
-      children: [],
-      internal: {
-        contentDigest: createContentDigest(radar),
-        type: 'TechnologyRadar'
-      }
-    };
-
-    createNode(radarNode);
-    createParentChildLink({parent: node, child: radarNode});
+  if (pluginOptions.layout) {
+    radar = createRadar(
+      pluginOptions.radarName,
+      node.items,
+      pluginOptions.layout
+    );
+  } else {
+    radar = createRadar(
+      pluginOptions.radarName,
+      node.items,
+      pluginOptions.layout
+    );
   }
+
+  const {createNode, createParentChildLink} = actions;
+
+  const radarNode = {
+    ...radar,
+    id: createNodeId(`${node.id} Radar`),
+    children: [],
+    internal: {
+      contentDigest: createContentDigest(radar),
+      type: `TechnologyRadar`
+    }
+  };
+
+  createNode(radarNode);
+  createParentChildLink({parent: node, child: radarNode});
 };
 
 exports.onCreateNode = onCreateNode;
+exports.unstable_shouldOnCreateNode = unstable_shouldOnCreateNode;
